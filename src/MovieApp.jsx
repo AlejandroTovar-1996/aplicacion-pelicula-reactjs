dbde9f0a7ec3fa6980a1f19608b4d34929594cc9
Alejandro <Alejandrojtovar.04@gmail.com>
2025-09-20 01:08:00 -0300

    Actualice MovieApp.jsx y MovieApp.js

--- src/MovieApp.jsx
+++ src/MovieApp.jsx
@@ -1,56 +1,71 @@
-import { useState } from 'react'
-import './MovieApp.css'
+import { useState } from 'react';
+import './MovieApp.css';
 
 export const MovieApp = () => {
+  const [search, setSearch] = useState('');
+  const [movieList, setMovieList] = useState([]);
 
-    const [search, setSearch] = useState('')
-    const [movieList, setMovieList] = useState([])
+  const urlBase = 'https://api.themoviedb.org/3/search/movie';
+  const API_KEY = 'fe801952f71109727d8eee42d93a6a77';
 
-    const urlBase = 'https://api.themoviedb.org/3/search/movie'
-    const API_KEY = 'fe801952f71109727d8eee42d93a6a77'
+  const handleInputChange = ({ target }) => {
+    setSearch(target.value);
+  };
 
-     const handleInputChange = ({ target }) => {
-        setSearch(target.value)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchMovies();
+  };
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        `${urlBase}?query=${search}&api_key=${API_KEY}&language=es-ES`
+      );
+      const data = await response.json();
+      setMovieList(data.results);
+    } catch (error) {
+      console.error('Ha ocurrido el siguiente error: ', error);
     }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        fetchMovies()
-    }
+  return (
+    <div className='container'>
+      <h1>Buscador de Películas</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Escribí una película'
+          value={search}
+          onChange={handleInputChange}
+        />
+        <button>Buscar</button>
+      </form>
 
-    const fetchMovies = async () => {
-        try {
-            const response = await fetch(`${urlBase}?query=${search}&api_key=${API_KEY}&language=es-ES`)
-            const data = await response.json()
-            setMovieList(data.results)
-        } catch (error) {
-            console.error('Ha ocurrido el siguiente error: ', error)
-        }
-    }
-
-    return (
-        <div className='container'>
-            <h1>Buscador de Películas</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder='Escribí una película'
-                    value={search}
-                    onChange={handleInputChange}
-                />
-                <button>Buscar</button>
-            </form>
-            {movieList &&
-                <div className='movie-list'>
-                    {movieList.map(movie => (
-                        <div key={movie.id} className='movie-card'>
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                            <h2>{movie.title}</h2>
-                            <p>{movie.overview}</p>
-                        </div>
-                    ))}
-                </div>
-            }
+      {movieList && (
+        <div className='movie-list'>
+          {movieList.map((movie) => (
+            <div key={movie.id} className='movie-card'>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <h2>{movie.title}</h2>
+              <p>{movie.overview}</p>
+              <button
+                onClick={() =>
+                  window.open(
+                    `https://www.themoviedb.org/movie/${movie.id}`,
+                    '_blank'
+                  )
+                }
+              >
+                Ver más
+              </button>
+            </div>
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
